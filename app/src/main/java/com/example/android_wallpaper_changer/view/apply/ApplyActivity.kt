331c5754c51a3089a5b9ec7671796b70bffb33e6
김효선 <s19046@gsm.hs.kr>
2021-08-13 13:55:47 +0900

    test

---
+++ app/src/main/java/com/example/android_wallpaper_changer/view/apply/ApplyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_wallpaper_changer.view.apply
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_wallpaper_changer.R
+
+class ApplyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apply)
+    }
+}
